35b2553315edfb5f715d3067e5389f7339682ab9
Anakvyas <anakvyas2004@gmail.com>
2026-09-19 16:34:06 +0530

    Remove dead legacy Node.js backend/src (superseded by FastAPI)

--- backend/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const env = require('./config/env');
-
-app.listen(env.port, () => {
-  console.log(`Smart Picker backend listening on http://localhost:${env.port}`);
-});
